6010e3faa8eb8b2c748ae397cf15184e509e16a5
Malin Larsson <malin.c.larsson@me.com>
2019-09-25 13:44:07 +0200

    dependencies

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const Thanks = () => {
     
     return (
+        <div>
+            <Navbar />
+
             <h1>Thank you for shopping!</h1>
+        </div>
     )
 }
 export default Thanks
