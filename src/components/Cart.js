fb966c7dc951d59547fac9deed6b102bc64f8f67
Malin Larsson <malin.c.larsson@me.com>
2019-09-27 15:19:06 +0200

    working on cart - prices

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -36,20 +36,21 @@ const Cart = () => {
           </thead>
 
           <tbody>
-          {result.length && result.map(cart => ( // Warning: Text nodes cannot appear as a child of <tbody>.
+          {/*} // IF-sats för att kolla ifall items redan finns i cart/samma --> slå ihop antal och pris */}
+          {result.length && result.map(cart => ( // Warning: Text nodes cannot appear as a child of <tbody>
             <tr>
               <td>{cart.name}</td>
               <td><input type='number' min={1} placeholder='1' className='quantity' /></td>
               <td>{cart.price} sek</td>
               <td>Slutsumma</td>
               <td className='nope' onClick={() => removeItem(cart)}>{' '}<FaTrashAlt />{' '}</td>
-
             </tr>
           ))}
           </tbody>
 
         </table>
       </div>
+      <h2 className='total'> Total cost: </h2>
 
       <Link to='/Checkout'>
         <button className='checkout'>Go to checkout</button> {/* onClick={goToCheckout} */}
